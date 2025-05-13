c9a32c7512ab8b0032520887f088e42612465ed5
Valentin <latronixo@gmail.com>
2025-05-13 20:52:43 +0300

    Initial Commit

---
+++ 20.GuessTheFlag/_0_GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_GuessTheFlagApp.swift
+//  20.GuessTheFlag
+//
+//  Created by Валентин on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct _0_GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
