f2034e70b153a45c89ba3676676935341c1e5056
Valentin <latronixo@gmail.com>
2025-05-14 16:25:05 +0300

    20. 4/9 Изучаем Image

--- 20.GuessTheFlag/ContentView.swift
+++ 20.GuessTheFlag/ContentView.swift
@@ -9,21 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Button("Button 1") { }
-                .buttonStyle(.bordered)
-
-            Button("Button 2", role: .destructive) { }
-                .buttonStyle(.bordered)
-
-            Button("Button 3") { }
-                .buttonStyle(.borderedProminent)
-                .tint(.indigo)
-
-            Button("Button 4", role: .destructive) { }
-                .buttonStyle(.borderedProminent)
-
-        }
+        Image(systemName: "pencil.circle")
+            .foregroundStyle(.red)
+            .font(.largeTitle)
     }
 
     func executeDelete () {
