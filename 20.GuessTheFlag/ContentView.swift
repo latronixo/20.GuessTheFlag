d06cdf3b365f7951ae34c55a40f49b080cdabf9d
Valentin <latronixo@gmail.com>
2025-05-14 20:33:52 +0300

    21. 9/9 Модернизировал дизайн, в том числе чтобы норм смотрелся на всех устройствах

--- 20.GuessTheFlag/ContentView.swift
+++ 20.GuessTheFlag/ContentView.swift
@@ -16,34 +16,58 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 700)
                 .ignoresSafeArea()
             
-            VStack (spacing: 30) {
-                VStack {
-                    Text("Нажмите на флаг")
-                        .foregroundStyle(.white)
-                        .font(.subheadline.weight(.heavy))
-                    
-                    Text(countries[correctAnswer])
-                        .foregroundStyle(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
-                ForEach(0..<3) { number in
-                    Button {
-                        flagTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .clipShape(.capsule)
-                            .shadow(radius: 5)
+            VStack {
+                Spacer()
+                
+                Text("Угадай флаг")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                VStack (spacing: 15) {
+                    VStack {
+                        Text("Нажмите на флаг")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        
+                        Text(countries[correctAnswer])
+                            //.foregroundStyle(.white)
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Ваш счет: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
-            .alert(scoreTitle, isPresented: $showingScore) {
-                Button("Продолжить", action: askQuestion)
-            } message: {
-            Text("Ваш счет: ХХХ")
-            }
+            .padding()
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Продолжить", action: askQuestion)
+        } message: {
+        Text("Ваш счет: ХХХ")
         }
     }
 
