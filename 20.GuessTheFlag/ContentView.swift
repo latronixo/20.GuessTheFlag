2334852193b681c75131f7e773e42c93526405ec
Valentin <latronixo@gmail.com>
2025-05-14 19:25:14 +0300

    20. 8/9 Добавил алерт. Для флагов скругление углов и тень. Увеличил и пожирнил надписи

--- 20.GuessTheFlag/ContentView.swift
+++ 20.GuessTheFlag/ContentView.swift
@@ -8,34 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
-    private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Russia", "US"]
-    private var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Russia", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var body: some View {
         ZStack {
-            Color.blue
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack (spacing: 30) {
                 VStack {
                     Text("Нажмите на флаг")
                         .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+                    
                     Text(countries[correctAnswer])
                         .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
                 }
                 ForEach(0..<3) { number in
                     Button {
-                        //событие нажатия на флаг
+                        flagTapped(number)
                     } label: {
                         Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
                     }
                 }
+            }
+            .alert(scoreTitle, isPresented: $showingScore) {
+                Button("Продолжить", action: askQuestion)
+            } message: {
+            Text("Ваш счет: ХХХ")
             }
         }
     }
 
-    func executeDelete () {
-        print("Удалено")
+    //событие тапа на флаг
+    func flagTapped (_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Верно"
+        } else {
+            scoreTitle = "Не верно"
+        }
+        
+        //показать алерт
+        showingScore = true
+    }
+    
+    //новый раунд
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
