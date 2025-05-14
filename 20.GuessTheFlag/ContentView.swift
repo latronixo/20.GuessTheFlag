165b6a96b421a0a1714f6b9beafd24d244328c6e
Valentin <latronixo@gmail.com>
2025-05-14 17:13:10 +0300

    20. 5/9 Прорешал алерты

--- 20.GuessTheFlag/ContentView.swift
+++ 20.GuessTheFlag/ContentView.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        Button {
-            print("Button was tapped")
-        } label: {
-            VStack (alignment: .leading) {
-                Color.yellow
-                Text("Hello, world!")
-                Text("This is another text view")
-            }
-            .background(.red)
+        Button("Show alert"){
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        } message: {
+            Text("Please read this")
         }
     }
 
